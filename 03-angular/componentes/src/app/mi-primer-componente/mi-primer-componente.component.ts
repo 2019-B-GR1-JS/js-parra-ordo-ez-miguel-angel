926ccc0f1c2ff1a4818d3f64c588ec17abc79352
Miguel-Parra <miguelts644@hotmail.com>
2019-12-02 16:26:45 -0500

    examen

--- 03-angular/componentes/src/app/mi-primer-componente/mi-primer-componente.component.ts
+++ 03-angular/componentes/src/app/mi-primer-componente/mi-primer-componente.component.ts
@@ -7,8 +7,12 @@ import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angula
 })
 export class MiPrimerComponenteComponent
   implements OnInit, OnDestroy {
-  ancho = 200;
-  alto = 200;
+  ancho: number;
+  alto: number;
+  suma: number;
+  resta: number;
+  multiplicacion: number;
+  division: number;
 
   @Input()
   titulo: string;
@@ -22,39 +26,82 @@ export class MiPrimerComponenteComponent
   @Input()
   textoBoton: string;
 
+  @Input()
+  numeroUno: number;
+
+  @Input()
+  numeroDos: number;
+
   @Output()// Evento
   public cambioSueldo = new EventEmitter();
+  @Output()
+  mostrarResultados = new EventEmitter();
 
 
   constructor() {
-    console.log('Instanciando')
+    console.log('Instanciando');
   }
 
   ngOnInit(): void { // La clase está lista
-    console.log('Esta listo');
+    this.ancho = 200;
+    this.alto = 200;
+    this.suma = 0;
+    this.resta = 0;
+    this.multiplicacion = 0;
+    this.division = 0;
+    /*console.log('Esta listo');
     console.log(this.titulo);
     console.log(this.textoImagen);
-    console.log(this.textoBoton);
+    console.log(this.textoBoton);*/
   }
 
-  ngOnDestroy(): void { //la clase esta destruida
+  ngOnDestroy(): void { // la clase esta destruida
   }
 
-  saludar(){
-alert("HOLAAAAAAAAAAAAAAAAAAAAAAAA..................")
+  saludar() {
+    alert ('HOLAAAAAAAAAAAAAAAAAAAAAAAA..................');
 
   }
 
-  aumentarSueldo(){
-    //this.textoBoton = this.textoBoton + 1;
+  aumentarSueldo() {
+    // this.textoBoton = this.textoBoton + 1;
     this.textoBoton = (Number(this.textoBoton) + 1).toString();
     this.cambioSueldo.emit(this.textoBoton);
-    //this.textoBoton = (+this.textoBoton +1).toString();
+    // this.textoBoton = (+this.textoBoton +1).toString();
   }
 
   aumentarAltoAncho() {
     this.ancho = this.ancho + 15;
     this.alto = this.alto + 20;
   }
+
+  escucharNumero(event) {
+    console.log('Evento ', event);
+    this.numeroUno = Number(event.srcElement.value);
+    this.calcular();
+  }
+  escucharNumeroDos(event) {
+    this.numeroDos = Number(event.srcElement.value);
+    console.log(this.numeroDos);
+    // this.enviarNumeroAOperaciones.emit(this.numDos);
+    this.calcular();
+  }
+
+  calcular() {
+    const numUno = Number(this.numeroUno);
+    const numDos = Number(this.numeroDos);
+
+
+    this.suma = numUno + numDos;
+    console.log('S: ', numUno + numDos);
+    this.resta = numUno - numDos;
+    console.log('R: ', numUno - numDos);
+    this.multiplicacion = numUno * numDos;
+    console.log('M: ', numUno * numDos);
+    this.division = numUno / numDos;
+    console.log('D: ', numUno / numDos);
+
+  }
+
 
 }
